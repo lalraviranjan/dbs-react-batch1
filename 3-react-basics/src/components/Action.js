1d6a67640b788c67ac0a65df35e48f98347334d5
Nagabhushanam <nagabhushanamn@gmail.com>
2019-04-05 10:09:50 +0530

    soln

--- 3-react-basics/src/components/Action.js
+++ 3-react-basics/src/components/Action.js
@@ -9,18 +9,22 @@ class Action extends Component {
         let { count } = this.state;
         count += 1;
         this.setState({ count })
-        let { onAction } = this.props;
+        let { onAction, value } = this.props;
         if (onAction) {
-            onAction();
+            onAction({ amt: count * value });
         }
     }
     render() {
         let { value } = this.props;
         let { count } = this.state;
+        let className = `btn ${value < 0 ? 'btn-danger' : 'btn-info'}`
         return (
             <div className="action">
                 <div className="card card-body">
-                    <button onClick={e => this.incremetCount()} className="btn btn-primary">{value}</button>
+                    <button onClick={e => this.incremetCount()}
+                        className={className}>
+                        {value}
+                    </button>
                     <hr />
                     <span className="badge badge-dark">{count}</span>
                 </div>
--- 3-react-basics/src/components/ActionBox.js
+++ 3-react-basics/src/components/ActionBox.js
@@ -7,9 +7,9 @@ class ActionBox extends Component {
     state = {
         totalCount: 0
     }
-    incrementTotalCount() {
+    incrementTotalCount(e) {
         let { totalCount } = this.state;
-        totalCount += 1;
+        totalCount += e.amt;
         this.setState({ totalCount })
     }
     render() {
@@ -18,8 +18,8 @@ class ActionBox extends Component {
             <div className="card">
                 <div className="card-header">Action Box : <span className="badge badge-warning">{totalCount}</span></div>
                 <div className="card-body">
-                    <Action value={10} onAction={e => this.incrementTotalCount()} />
-                    <Action value={-10} onAction={e => this.incrementTotalCount()} />
+                    <Action value={10} onAction={e => this.incrementTotalCount(e)} />
+                    <Action value={-10} onAction={e => this.incrementTotalCount(e)} />
                 </div>
             </div>
         );
